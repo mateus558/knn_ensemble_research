1b426a11f09723a37e3d4ddf138cae6d9029446f
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2021-08-17 19:41:18 -0300

    Update results.

---
+++ results/execution/opt_fetch_results.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import os
+
+
+out_dir = "knn_optm_res"
+if not os.path.isdir(out_dir):
+    os.mkdir(out_dir)
+
+data = pd.read_csv("results_qpoptm.csv", sep=";", skiprows=[0])
+data = data.drop('time', axis=1)
+print(data.head())
+
+k_groups = data.groupby(by=["k"]).groups
+for key, items in k_groups.items():
+    k_res = data.iloc[items] 
+    k_res.to_csv(out_dir+f"/{key}_{out_dir}.csv", sep=";", index=False)
