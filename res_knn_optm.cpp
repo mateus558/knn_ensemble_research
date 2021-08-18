8fd3aa644c1c9fe82973d7eeb872e11e988a214d
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2021-08-18 15:30:50 -0300

    Update results.

--- res_knn_optm.cpp
+++ res_knn_optm.cpp
@@ -14,8 +14,8 @@ std::ofstream output;
 std::ofstream output_csv;
 
 void prepare_out_files(){
-    output.open("../results/execution/output_qpoptm_only_one_opt.txt");
-    output_csv.open("../results/execution/results_qpoptm_only_one_opt.csv");
+    output.open("../results/execution/output_qpoptm_only_one_opt_multacc.txt");
+    output_csv.open("../results/execution/results_qpoptm_only_one_opt_multacc.csv");
 
     if(!output.is_open() || !output_csv.is_open()){
         std::cerr << "The output file could not be open." << std::endl;
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]){
         std::vector<std::future<void>> futures(ks.size());
         std::transform(ks.begin(), ks.end(), futures.begin(), [](std::pair<mltk::Data<double>, size_t>& data_pair){
             auto run_valid = [](std::pair<mltk::Data<double>, size_t> data_pair){
-                mltk::ensemble::KNNEnsembleOptm<double> knn_ensemb(data_pair.first, data_pair.second, false, 10, 0, 0);
+                mltk::ensemble::KNNEnsembleOptm<double> knn_ensemb(data_pair.first, data_pair.second, true, 10, 0, 0);
                 mltk::Timer timer;
 
                 auto report = mltk::validation::kkfold(data_pair.first, knn_ensemb, 10, 10, true,
